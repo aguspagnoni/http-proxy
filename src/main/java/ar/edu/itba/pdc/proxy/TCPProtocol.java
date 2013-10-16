fec18d3ee75a748c6a1763463a346be8d9ee2f39
Agustin Pagnoni <aguspagnoni@hotmail.com>
2013-10-16 18:50:39 -0300

    starting

---
+++ src/main/java/ar/edu/itba/pdc/proxy/TCPProtocol.java
@@ -0,0 +1,10 @@
+package ar.edu.itba.pdc.proxy;
+
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+
+public interface TCPProtocol {
+      void handleAccept(SelectionKey key) throws IOException;
+      void handleRead(SelectionKey key) throws IOException;
+      void handleWrite(SelectionKey key) throws IOException;
+}
