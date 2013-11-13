3460bab79ade2e7d5e3f54ff5387f681b9bab436
Federico Bond <federicobond@gmail.com>
2013-11-13 01:40:20 -0300

    Channel Buffer removed useless methods

--- src/main/java/ar/edu/itba/pdc/proxy/ChannelBuffers.java
+++ src/main/java/ar/edu/itba/pdc/proxy/ChannelBuffers.java
@@ -15,7 +15,7 @@ public class ChannelBuffers {
 	public ChannelBuffers() {
 		initializeMap(ByteBuffer.allocate(BUFFER_SIZE),
 				ByteBuffer.allocate(BUFFER_SIZE));
-		req=new PDCRequest();
+		req = new PDCRequest();
 	}
 
 	public ChannelBuffers(ByteBuffer readBuffer, ByteBuffer writeBuffer) {
@@ -37,17 +37,6 @@ public class ChannelBuffers {
 	}
 
 	/**
-	 * Sets one of the two buffers given the BufferType
-	 * 
-	 * @param type
-	 * @param buffer
-	 */
-
-	public void setBuffer(BufferType type, ByteBuffer buffer) {
-		buffers.put(type, buffer);
-	}
-
-	/**
 	 * Returns one of the two buffers given the BufferType
 	 * 
 	 * @param type
@@ -58,112 +47,11 @@ public class ChannelBuffers {
 	}
 
 	/**
-	 * Writes the given byte array in one of the buffers depending on which
-	 * BufferType (read or write) is received by parameter.
-	 * 
-	 * @param type
-	 * @param bytes
-	 */
-
-	public void writeToBuffer(BufferType type, byte[] bytes) {
-		ByteBuffer buf = (type == BufferType.read) ? buffers
-				.get(BufferType.read) : buffers.get(BufferType.write);
-		if (buf.capacity() - buf.remaining() < bytes.length) {
-			buf.flip();
-			buf = ByteBuffer.allocate(buf.capacity() + bytes.length).put(buf);
-			if (type == BufferType.read) {
-				setBuffer(BufferType.read, buf);
-			} else {
-				setBuffer(BufferType.write, buf);
-			}
-		}
-		buf.put(bytes);
-	}
-
-	/**
-	 * Clears the given buffer (read or write)
-	 * 
-	 * @param type
-	 */
-
-	public void clearBuffer(BufferType type) {
-		if (buffers.get(type) != null)
-			buffers.get(type).clear();
-	}
-
-	/**
-	 * Compacts the given buffer (read or write)
-	 * 
-	 * @param type
-	 */
-
-	public void compactBuffer(BufferType type) {
-		if (buffers.get(type) != null)
-			buffers.get(type).compact();
-	}
-
-	/**
-	 * Flips the given buffer (read or write)
-	 * 
-	 * @param type
-	 */
-
-	public void flipBuffer(BufferType type) {
-		if (buffers.get(type) != null)
-			buffers.get(type).flip();
-	}
-
-	/**
-	 * Returns the buffer array of the given buffer (read or write)
-	 * 
-	 * @param type
-	 */
-
-	public byte[] getBufferArray(BufferType type) {
-		if (buffers.get(type) != null)
-			return buffers.get(type).array();
-		return null;
-	}
-
-	/**
-	 * Expands the given buffer (read or write)
-	 * 
-	 * @param type
-	 */
-
-	public void expandBuffer(BufferType type) {
-		ByteBuffer buf = getBuffer(type);
-		buf.flip();
-		setBuffer(type, ByteBuffer.allocate(buf.capacity() * 2).put(buf));
-	}
-
-	/**
-	 * Returns true if the buffer has remaining information
-	 * 
-	 * @param type
-	 */
-
-	public boolean hasRemainingFor(BufferType type) {
-		return buffers.get(type) != null && buffers.get(type).hasRemaining();
-	}
-
-	/**
-	 * Returns true if the buffer has remaining information
-	 * 
-	 * @param type
-	 */
-
-	public boolean hasInformationFor(BufferType type) {
-		return buffers.get(type) != null
-				&& (buffers.get(type).capacity() != buffers.get(type)
-						.remaining());
-	}
-	
-	/**
 	 * Returns the PDCRequest asociated to that channelBuffers
+	 * 
 	 * @return
 	 */
-	public PDCRequest getRequest(){
+	public PDCRequest getRequest() {
 		return this.req;
 	}
 }
